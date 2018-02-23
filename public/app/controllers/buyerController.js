d2627f46db1618157113fa2d065c394640d17e03
harunkelesoglu <harunkelesoglu_@hotmail.com>
2018-02-23 03:00:25 +0300

    changed the api url have https instead of http

--- public/app/controllers/buyerController.js
+++ public/app/controllers/buyerController.js
@@ -3,7 +3,7 @@ angular.module('buyerController',['ngMap','dataProviderService'])
            const user = JSON.parse($window.localStorage.getItem('user'));
            
            //render user information 
-           $scope.googleMapsUrl = "http://maps.google.com/maps/api/js?key=AIzaSyCr0NW4ERXAoCLB7i15PD7lSDw4yz59nno";
+           $scope.googleMapsUrl = "https://maps.google.com/maps/api/js?key=AIzaSyCr0NW4ERXAoCLB7i15PD7lSDw4yz59nno";
            $scope.location = user.location;
            $scope.user = user.name+" "+user.surname;
            $scope.zoom = 12;
